5f8f6383c72c2df40efa4358b74b2d0d56f1d0bd
Milorad Vojnovic <milekuglas@gmail.com>
2018-11-12 11:49:52 +0100

    Create model examples

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from textx import metamodel_from_file
 
 metamodel = metamodel_from_file('metamodel/graphQL.tx')
+
+findOneUser = metamodel.model_from_file('metamodel/findOneUser.test')
+createNewUser = metamodel.model_from_file('metamodel/createNewUser.test')
+findAllUsers = metamodel.model_from_file('metamodel/findAllUsers.test')
