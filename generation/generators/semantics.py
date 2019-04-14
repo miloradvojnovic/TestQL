f1548eb3a6e6276d9c4d53a1040f60ead1d4fbfa
Milorad Vojnovic <milekuglas@gmail.com>
2019-04-14 22:13:57 +0200

    Add semantic analysis
    Closes #6

---
+++ generation/generators/semantics.py
@@ -0,0 +1,74 @@
+from textx import TextXSemanticError
+
+def request_obj_processor(request):
+    tag_values = set()
+    for att in request.attributes:
+        for arg in att.arguments:
+            check_request_tags(tag_values, arg)
+    return tag_values
+
+def check_request_tags(tag_values, arg):
+    if 'TagValue' in arg.value.__class__.__name__:
+        value = arg.value.name
+        if value in tag_values:
+            raise TextXSemanticError('Same Tag name (%s) for GraphQL arguments is not allowed.' % value)
+        else:
+            tag_values.add(value)
+    elif 'CompoundValue' in arg.value.__class__.__name__:
+        for argument in arg.value.arguments:
+            check_request_tags(tag_values, argument)
+
+def response_obj_processor(response):
+    tag_values = set()
+    for member in response.json.members:
+        check_response_tags(tag_values, member.value)
+    return tag_values
+
+def check_response_tags(tag_values, memb_value):
+    if 'TagValue' in memb_value.__class__.__name__:
+        value = memb_value.name
+        if value in tag_values:
+            raise TextXSemanticError('Same Tag name (%s) for response is not allowed.' % value)
+        else:
+            tag_values.add(value)
+    elif 'JsonObject' in memb_value.__class__.__name__:
+        for member in memb_value.members:
+            check_response_tags(tag_values, member.value)
+    elif 'JsonArray' in memb_value.__class__.__name__:
+        mult = memb_value.multiplier
+        if mult is not None:
+            if 'TagValue' in mult.value.__class__.__name__:
+                value = mult.value.name
+                if value in tag_values:
+                    raise TextXSemanticError('Same Tag name (%s) for response is not allowed.' % value)
+                else:
+                    tag_values.add(value)
+        for value in memb_value.values:
+            check_response_tags(tag_values, value)    
+
+def examples_obj_processor(examples): 
+    values = set()
+    if examples.attributes.attributes[0].name != 'should':
+        raise TextXSemanticError('First attribute must be should.')
+    for att in examples.attributes.attributes:
+        name = att.name
+        if name in values:
+            raise TextXSemanticError('Same Attribute name (%s) for examples is not allowed.' % name)
+        else:
+            values.add(name)
+    return values
+
+def check_some_semantics(model, metamodel):
+    tag_values = model.request
+    example_values = set()
+    for case in model.cases:
+        tag_values = tag_values.union(case.response)
+        example_values = example_values.union(case.examples)
+        example_values.remove('should')
+
+    for tag_val in tag_values:
+        if tag_val not in example_values:
+            raise TextXSemanticError('Tag value (%s) is not present in examples.' % tag_val)
+    for ex_val in example_values:
+        if ex_val not in tag_values:
+            raise TextXSemanticError('Tag value (%s) is not present in request nor responses.' % ex_val)
--- main.py
+++ main.py
@@ -5,9 +5,19 @@ import click
 
 from generation.generators.main_generator import MainGenerator
 from generation.generators.test_spec_generator import TestSpecGenerator
+from generation.generators.semantics import *
+
+
 
 def get_model(path, models={}):
     metamodel = metamodel_from_file('metamodel/graphql.tx')
+    obj_processors = {
+        'Request': request_obj_processor, 
+        'Response': response_obj_processor, 
+        'Examples': examples_obj_processor
+        }
+    metamodel.register_obj_processors(obj_processors)
+    metamodel.register_model_processor(check_some_semantics)
     for r, d, f in os.walk(path):
         for file in f:
             package = None
